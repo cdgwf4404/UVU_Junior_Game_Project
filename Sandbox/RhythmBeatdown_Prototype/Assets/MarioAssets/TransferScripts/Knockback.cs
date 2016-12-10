ec7bd3078bab904fab0e312b3ac18e01a4e71950
Jeremy Graham <ss2jeremy@gmail.com>
2016-12-10 16:24:23 -0700

    Camera and Knockback
    Added a dynamic camera and knockback feature.

---
+++ Sandbox/RhythmBeatdown_Prototype/Assets/MarioAssets/TransferScripts/Knockback.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Knockback : MonoBehaviour {
+
+	public int projectileLayer = 12;
+
+	// THESE ARE THE VARIABLES YOU WILL NEED
+	public Rigidbody rb; // YOU WILL NEED THIS IF IT IS NOT ALREADY ASSIGNED
+	public float knockbackX = 0.5f; //THIS IS THE VARIABLE THAT YOU NEED TO CHANGE DEPENDING ON THE SIDE THEY ARE ON.
+	public float knockbackY = 0.5f;
+	private Vector2 knockbackSpot;
+	public float speed = 2f;
+	public float knockbackTime= 1f;
+
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.layer == projectileLayer) 
+		{
+			// NEEDED CODE: This is the code that you will add to your detection script.
+			knockbackSpot = new Vector3 (transform.position.x + knockbackX, transform.position.y + knockbackY, 1);
+			float elapsedTime = 0;
+			while (elapsedTime < knockbackTime) 
+			{
+				transform.position = Vector3.Lerp (transform.position, knockbackSpot, speed*Time.deltaTime);
+				elapsedTime += Time.deltaTime;
+			}
+			// We did this because for some reason the knockback was moving forward.
+			transform.position = new Vector3 (transform.position.x, transform.position.y, 1);
+
+			//CODE ENDS HERE
+		}
+		
+	}
+
+	void Start() 
+	{
+		rb = GetComponentInParent<Rigidbody> ();
+
+	}
+
+}
---
+++ Sandbox/RhythmBeatdown_Prototype/Assets/cameraMidpoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMidpoint : MonoBehaviour {
+
+	public float cameraZOffset, cameraYOffeset, cameraRotate, speed;
+	private float distance, midpoint, xDistance;
+	private Vector3 cameraPosition;
+	public GameObject player1, player2;
+
+	void Start () 
+	{
+		//Set the y position and x rotation here
+		transform.position = new Vector3 ( transform.position.x, cameraYOffeset, transform.position.z);
+		transform.eulerAngles = new Vector3 (cameraRotate, transform.rotation.y, transform.rotation.z);
+	}
+
+	void FixedUpdate () 
+	{
+		//Midpoint equals the midpoint between the two players - Distance is the distance between the players and will be used for cameraZPosition
+		midpoint = player1.transform.position.x + (player2.transform.position.x - player1.transform.position.x) / 2; 
+		distance = Mathf.Abs (Vector3.Distance (player1.transform.position, player2.transform.position));
+
+		//Set cameraPosition according to the variables above and lerp between  those positions
+		cameraPosition = new Vector3 ( midpoint, transform.position.y, -distance + cameraZOffset);
+		transform.position = Vector3.Lerp (transform.position, cameraPosition, speed*Time.deltaTime);
+	}
+}
